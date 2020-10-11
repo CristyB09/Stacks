51fb506e0d8ab08e5947ef33b7d6155713d83a4e
CristyB09 <bontecristian@yahoo.com>
2020-10-11 16:50:00 +0100

    Add file via download

---
+++ src/StackInterface.java
@@ -0,0 +1,33 @@
+public interface StackInterface<T> {
+
+    public void push(T newEntry);
+     /* add a new entry to the top of the stack
+        @param (T) newEntry - the entry to be added
+      */
+
+
+    public T pop();
+    /* remove an entry from the top of the stack
+    throw EmptyStackException if called on an empty stack
+    @return (T) - the value that was on top of the stack
+     */
+
+
+    public T peek();
+    /* return but don't remove the entry on top of the stack
+    throw EmptyStackException if called on an empty stack
+    @return (T) - the value at the top of the stack
+     */
+
+    public boolean isEmpty();
+    /* return true if the stack is empty, false otherwise
+     */
+
+    public void clear();
+    /* remove all entries from the stack
+     */
+
+}
+
+
+
